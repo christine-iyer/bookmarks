e7b57475ded938e9f288b6d62795c457a1e63785
chrisIyer <hellohotbox420@gmail.com>
2023-01-28 10:16:32 -0500

    saving

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,14 @@ import React from 'react'
 const Header = () => {
   return (
     <div className={styles.header}>
-     <h1 className={styles.title}>Dinner Notes</h1>
-     <h3 className={styles.subtitle}>Pin Now Share Later</h3>
+     <h1 className={styles.title}>
+      <strong>Dinner Notes </strong>Pin Now Share Later
+       
+       </h1>
      
      </div>
   )
 }
 
 export default Header
+
